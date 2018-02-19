f0fc61c1e530462f9f576642c2701e953a1abe63
Dharmaraj <dharmaraj.techie@gmail.com>
2018-02-19 14:49:23 +0530

    testing Acess modifiers
    tested acess modifier and  complexity involved with java nessted classes

--- Inner Classes Example/src/MainClass.java
+++ Inner Classes Example/src/MainClass.java
@@ -2,23 +2,19 @@
 public class MainClass {
 	static int i=2;
 	
-	static {
-		System.out.println("static field "+i); 
-	}
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//			System.out.println("This is main class ");
-		
-			System.out.println("main method "+i); 
+			System.out.println("This is main class ");
 	
-//			
-//			OuterClass outerClassObject = new OuterClass();
-//			outerClassObject.outerClassMethod();
-//			
-//			OuterClass.InnerClass innerClassObj = outerClassObject.new InnerClass();
-//			innerClassObj.innerClassMethod();
+			OuterClass outerClassObject = new OuterClass();
+			outerClassObject.outerClassMethod();
+			outerClassObject.outerClassMethod("called from main class");
+			
+			OuterClass.InnerClass innerClassObj = outerClassObject.new InnerClass();
+			innerClassObj.innerClassMethod();
+			
+			innerClassObj.callOuterClassMethodFromInnerClass();
+		
 	}
 }
 
--- Inner Classes Example/src/OuterClass.java
+++ Inner Classes Example/src/OuterClass.java
@@ -4,12 +4,19 @@ public class OuterClass {
 	public void outerClassMethod(){
 		System.out.println("This is outer class method called");
 	}
+	protected void outerClassMethod(String s){
+		System.out.println("This is outer class method called with params :"+s);
+	}
 	
-
 	class InnerClass{
 		public void innerClassMethod(){
 			System.out.println("This is inner class method called");
 		}
-	}	
+		
+		public void callOuterClassMethodFromInnerClass(){
+			outerClassMethod("called from innerClass");
+		}
+	}
 }
+
 
