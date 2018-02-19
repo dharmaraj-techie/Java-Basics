111255def59ec88e780adfdf918a2de91feb1f24
Dharmaraj <dharmaraj.techie@gmail.com>
2018-02-19 14:18:58 +0530

    which is initiated first
    i tested to find which field get initiated first static or  declaration.

--- Inner Classes Example/src/MainClass.java
+++ Inner Classes Example/src/MainClass.java
@@ -1,18 +1,26 @@
 
 public class MainClass {
+	static int i=2;
+	
+	static {
+		System.out.println("static field "+i); 
+	}
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("This is main class ");
-			
-			OuterClass outerClassObject = new OuterClass();
-			outerClassObject.outerClassMethod();
+//			System.out.println("This is main class ");
+		
+			System.out.println("main method "+i); 
+	
+//			
+//			OuterClass outerClassObject = new OuterClass();
+//			outerClassObject.outerClassMethod();
+//			
+//			OuterClass.InnerClass innerClassObj = outerClassObject.new InnerClass();
+//			innerClassObj.innerClassMethod();
 	}
 }
 
- class OuterClass{
-	public void outerClassMethod(){
-		System.out.println("This is outer class");
-	}
-}
+ 
 
---
+++ Inner Classes Example/src/OuterClass.java
@@ -0,0 +1,15 @@
+
+public class OuterClass {
+	
+	public void outerClassMethod(){
+		System.out.println("This is outer class method called");
+	}
+	
+
+	class InnerClass{
+		public void innerClassMethod(){
+			System.out.println("This is inner class method called");
+		}
+	}	
+}
+
